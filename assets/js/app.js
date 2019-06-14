711ef788ea0f42cc34a4364b01d0740eca5c28cb
S.M. Rafiz <rafiz@codexin.com>
2019-06-14 19:36:27 +0600

    Fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,48 @@
+"use strict";
+
 var data = {
-	keyPlay: function(key, audio) {
-		key.classList.add('playing');
-		audio.currentTime = 0;
-		audio.play();
-	},
+  keyPlay: function keyPlay(key, audio) {
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  },
+  playSound: function playSound(e) {
+    var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+    var key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
 
-	playSound: function(e) {
-		var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-		var key = document.querySelector('.key[data-key="' + e.keyCode + '"]');
+    if (!audio) {
+      return;
+    }
 
-		if (!audio) {
-			return;
-		}
+    data.keyPlay(key, audio);
+  },
+  clickSound: function clickSound(e) {
+    var keyCode = e.target.parentNode.getAttribute('data-key');
+    var audio = document.querySelector('audio[data-key="' + keyCode + '"]');
+    var key = document.querySelector('.key[data-key="' + keyCode + '"]');
 
-		data.keyPlay(key, audio);
-	},
+    if (!audio) {
+      return;
+    }
 
-	clickSound: function(e) {
-		var keyCode = e.target.parentNode.getAttribute('data-key');
-		var audio = document.querySelector('audio[data-key="' + keyCode + '"]');
-		var key = document.querySelector('.key[data-key="' + keyCode + '"]');
+    data.keyPlay(key, audio);
+  },
+  removeTransition: function removeTransition(e) {
+    if (e.propertyName !== 'transform') {
+      return;
+    }
 
-		if (!audio) {
-			return;
-		}
-
-		data.keyPlay(key, audio);
-	},
-
-	removeTransition: function(e) {
-		if (e.propertyName !== 'transform') {
-			return;
-		}
-
-		e.target.classList.remove('playing');
-	}
+    e.target.classList.remove('playing');
+  }
 };
-
 var view = {
-	eventListeners: function() {
-		var keys = document.querySelectorAll('.key');
-		keys.forEach(function(key) {
-			key.addEventListener('transitionend', data.removeTransition);
-		});
-
-		window.addEventListener('keydown', data.playSound);
-		window.addEventListener('click', data.clickSound);
-	}
+  eventListeners: function eventListeners() {
+    var keys = document.querySelectorAll('.key');
+    keys.forEach(function (key) {
+      key.addEventListener('transitionend', data.removeTransition);
+    });
+    window.addEventListener('keydown', data.playSound);
+    window.addEventListener('click', data.clickSound);
+  }
 };
-
 view.eventListeners();
